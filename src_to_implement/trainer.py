588e20dace59510166bca7a3ca044f0c374d5fb0
AaS654 <aaron.mj.schwarz@fau.de>
2025-02-02 21:23:36 +0100

    Update trainer.py

--- src_to_implement/trainer.py
+++ src_to_implement/trainer.py
@@ -29,7 +29,7 @@ class Trainer:
             
     def save_checkpoint(self, epoch):
         t.save({'state_dict': self._model.state_dict()}, 'checkpoints/checkpoint_{:03d}.ckp'.format(epoch))
-        # self.save_onnx('checkpoints/model.onnx')
+        self.save_onnx('checkpoints/model.onnx')
 
     def restore_checkpoint(self, epoch_n):
         ckp = t.load('checkpoints/checkpoint_{:03d}.ckp'.format(epoch_n), 'cuda' if self._cuda else None)
