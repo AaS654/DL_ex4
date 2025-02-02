febd5065be1e87239e3195a8fa11391df247bc70
AaS654 <aaron.mj.schwarz@fau.de>
2025-02-02 17:36:22 +0100

    Local files to setup new project

---
+++ src_to_implement/data.py
@@ -0,0 +1,54 @@
+from torch.utils.data import Dataset
+import torch
+from pathlib import Path
+from skimage.io import imread
+from skimage.color import gray2rgb
+import numpy as np
+import torchvision.transforms as tv
+import pandas as pd
+
+train_mean = [0.59685254, 0.59685254, 0.59685254]
+train_std = [0.16043035, 0.16043035, 0.16043035]
+
+
+class ChallengeDataset(Dataset):
+
+    def __init__(self, data: pd.DataFrame, mode: str): # mode ['val', 'train']
+        self.data = data
+        self.mode = mode
+
+
+        # Define transformations
+        if self.mode == 'train':
+            self.transform = tv.Compose([
+                tv.ToPILImage(),
+                tv.ToTensor(),
+                tv.Normalize(mean=train_mean, std=train_std)
+            ])
+        elif self.mode == 'val':  # Validation transformations
+            self.transform = tv.Compose([
+                tv.ToPILImage(),
+                tv.ToTensor(),
+                tv.Normalize(mean=train_mean, std=train_std)
+            ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        row = self.data.iloc[index]
+        image_path = row['filename']
+        labels = row[['crack', 'inactive']].values.astype(np.float32)
+
+        image = imread(image_path, as_gray=True)
+        image = gray2rgb(image)
+
+        if self.transform:
+            image = self.transform(image)
+
+        # Convert label to tensor
+        labels = torch.tensor(labels, dtype=torch.long)
+        # labels = np.array([row['crack'], row['inactive']])
+        return image, labels
+
+
---
+++ src_to_implement/train.py
@@ -0,0 +1,61 @@
+import torch as t
+from data import ChallengeDataset
+from trainer import Trainer
+from matplotlib import pyplot as plt
+import numpy as np
+import model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+# load the data from the csv file and perform a train-test-split
+# this can be accomplished using the already imported pandas and sklearn.model_selection modules
+# TODO
+
+
+csv_path = "data.csv"
+df = pd.read_csv(csv_path, sep=';')
+train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
+
+
+# set up data loading for the training and validation set each using t.utils.data.DataLoader and ChallengeDataset objects
+# TODO
+train_dataset = ChallengeDataset(data=train_df, mode='train')
+val_dataset = ChallengeDataset(data=val_df, mode='val')
+
+
+train_loader = t.utils.data.DataLoader(train_dataset, batch_size=50, shuffle=True)
+val_loader = t.utils.data.DataLoader(val_dataset, batch_size=50)
+
+
+# create an instance of our ResNet model
+# TODO
+resnet_model = model.ResNet() 
+
+# set up a suitable loss criterion (you can find a pre-implemented loss functions in t.nn)
+# set up the optimizer (see t.optim)
+# create an object of type Trainer and set its early stopping criterion
+# TODO
+criterion = t.nn.BCELoss() 
+optimizer = t.optim.Adam(resnet_model.parameters(), lr=0.001)
+
+trainer = Trainer(
+    model=resnet_model,
+    crit=criterion,
+    optim=optimizer,
+    train_dl=train_loader,
+    val_test_dl=val_loader,
+    cuda=False,  # Set to True if GPU is available
+    early_stopping_patience=3
+)
+
+
+res = trainer.fit(epochs=2)
+
+# plot the results
+plt.plot(np.arange(len(res[0])), res[0], label='train loss')
+plt.plot(np.arange(len(res[1])), res[1], label='val loss')
+plt.yscale('log')
+plt.legend()
+plt.savefig('losses.png')
+
